1c8ccad2275d2930a35629456c2a706f29c7223d
ogunmakindetobi <ogunmakindetobi@outlook.com>
2021-02-19 14:37:22 +0000

    task

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import os 
 from flask import Flask
-if os.path.exists("env.py"):
-    import env
+
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("")
+app.secret_key = os.environ.get('SECRET_KEY')
 
-@app.route("/")
-def hello():
-    return "Hello world... again!"
+mongo = PyMongo(app)
+starships = mongo.db.ships
+
+@app.route("/starships")
+def starships():
+    hyperdriveSorted = startships.find().sort({"hyperdrive": 1})
+    return jsonify(data)
 
 
 
